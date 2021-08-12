336c9dfa94801706da3e5d76ac3daa61362d38eb
st-182 <31894656+st-182@users.noreply.github.com>
2021-08-12 15:14:41 +0300

    post to mongodb added

---
+++ frontend/scripts/components/header.js
@@ -0,0 +1,35 @@
+//Variables
+//-taking existing elements
+const header = document.querySelector(`header`);
+const nav = () => {
+  header.innerHTML = `
+
+    <nav class="d-flex justify-content-center py-3">
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="./home.html" class="nav-link ${
+          document.title === "Home" ? "active" : ""
+        }" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="./addModel.html" class="nav-link ${
+          document.title === "Add Model" ? "active" : ""
+        }">Add Model</a></li>
+        <li class="nav-item"><a href="./addVehicle.html" class="nav-link ${
+          document.title === "Add Vehicle" ? "active" : ""
+        }">Add Vehicle</a></li>
+        <li class="nav-item"><a href="./viewModels.html" class="nav-link ${
+          document.title === "View Models" ? "active" : ""
+        }">View Models</a></li>
+        <li class="nav-item"><a href="./viewVehicles.html" class="nav-link ${
+          document.title === "View Vehicles" ? "active" : ""
+        }">View Vehicles</a></li>
+        <li class="nav-item"><a href="./about.html" class="nav-link ${
+          document.title === "About" ? "active" : ""
+        }">About</a></li>
+      </ul>
+    </nav>
+  
+`;
+
+  console.log(`too`);
+};
+
+export default nav;
